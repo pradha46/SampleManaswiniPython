1723aa6c9873f16388ee99d69ec337a196e01a9b
pradha46 <manaswinipradhan092@gmail.com>
2025-07-21 01:06:45 +0530

    Create Course.java

---
+++ Course.java
@@ -0,0 +1,16 @@
+{
+  "title": "Introduction to Web Development",
+  "description": "This course covers the fundamentals of web development, including HTML, CSS, and JavaScript.",
+  "price": 99.99,
+  "instructor_id": "instructor123", // Assuming instructor IDs are managed separately
+  "curriculum": [
+    {
+      "title": "Module 1: HTML Basics",
+      "description": "Learn the building blocks of web pages with HTML."
+    },
+    {
+      "title": "Module 2: CSS Styling",
+      "description": "Style your web pages with CSS."
+    }
+  ]
+}
